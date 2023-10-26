4e726daa5414d220119c62a1eabe9b85d8c44137
senya-koku <daiki.kokusenya.lp@ambl.co.jp>
2023-10-26 18:51:47 +0900

    fix: エラー対応

--- cdk/test_stack copy.py
+++ cdk/test_stack copy.py
@@ -6,7 +6,6 @@ USAGE_PLAN_ID = os.environ.get("USAGE_PLAN_ID")
 
 
 class ApiStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs):
         super().__init__(scope, id, env=core.Environment(region="ap-northeast-1"), **kwargs)
         
@@ -38,24 +37,17 @@ class ApiStack(core.Stack):
 
         # apiGatewayを用意する
         api = apigateway.RestApi(self, 'kokusenya_test',
-        deploy_options=apigateway.StageOptions(stage_name="dev"),
-        policy=policy  # この行を追加
-    )
-
-        # 既存のAPIキーを参照
-        existing_api_key = apigateway.ApiKey.from_api_key_id(self, "ExistingApiKey", api_key_id=API_KEY_ID)
-
+            deploy_options=apigateway.StageOptions(stage_name="dev")
+        )
+        
         # リソースパスの作成
         api_root = api.root.add_resource("api").add_resource("v1").add_resource("hello")
-
+        
         # リソースにLambda関数を結びつける
-        api_method = api_root.add_method("POST", apigateway.LambdaIntegration(lambda_function), 
-            api_key_required=True,  # APIキーが必要とする設定
+        api_root.add_method("POST", apigateway.LambdaIntegration(lambda_function), 
             request_parameters={
                 "method.request.querystring.id1": True
             }
         )
 
-        # APIキーをAPIステージに関連付ける
-        usage_plan = api.add_usage_plan("UsagePlan", api_key=existing_api_key)
-        usage_plan.add_api_stage(stage=api.deployment_stage)
+
