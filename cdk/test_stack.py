a143246434c01f284b06214816ff5e281f9494cc
senya-koku <daiki.kokusenya.lp@ambl.co.jp>
2023-10-26 14:29:28 +0900

    fix

--- cdk/test_stack.py
+++ cdk/test_stack.py
@@ -1,21 +1,19 @@
-from constructs import Construct
-from aws_cdk import (
-    Stack,
-    aws_lambda as _lambda,
-    aws_apigateway as apigateway,
-    aws_iam as iam
-)
-class ApiStack(Stack):
-    def __init__(self, scope:Construct, id:str, **kwargs):
+from aws_cdk import core
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_apigateway as apigateway
+from aws_cdk import aws_iam as iam
+
+class ApiStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
         # APIGatewayで呼ばれるlambda
         lambda_function = _lambda.Function(
             self, 'HelloHandler',
-            runtime = _lambda.Runtime.PYTHON_3_9,
-            function_name = 'Hello',
-            code = _lambda.Code.from_asset('lambda'),
-            handler = 'hello.handler',
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            function_name='Hello',
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
         )
 
         # IP制限のポリシーを定義
@@ -38,5 +36,7 @@ class ApiStack(Stack):
         # apiGatewayを用意する
         api = apigateway.LambdaRestApi(
             self, 'hello-api',
-            handler = lambda_function
+            handler=lambda_function,
+            policy=policy
         )
+
