d8362f5f7b2cc191901ae7598bedf8bb46e533df
senya-koku <daiki.kokusenya.lp@ambl.co.jp>
2023-10-26 14:06:18 +0900

    fix: 今回用に修正

---
+++ cdk/app.py
@@ -0,0 +1,15 @@
+from aws_cdk import core
+from cdk.test_stack import ApiStack
+
+class MyCdkApp(core.App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # スタックのインスタンスを作成し、アプリケーションに関連付ける
+        ApiStack(self, "MyApiStack")
+
+# アプリケーションのインスタンスを作成
+app = MyCdkApp()
+
+# アプリケーションの実行
+app.synth()
--- cdk/test_stack.py
+++ cdk/test_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigateway,
+    aws_iam as iam
 )
 class ApiStack(Stack):
     def __init__(self, scope:Construct, id:str, **kwargs):
@@ -11,10 +12,27 @@ class ApiStack(Stack):
         # APIGatewayで呼ばれるlambda
         lambda_function = _lambda.Function(
             self, 'HelloHandler',
-            runtime = _lambda.Runtime.PYTHON_3_8,
+            runtime = _lambda.Runtime.PYTHON_3_9,
             function_name = 'Hello',
             code = _lambda.Code.from_asset('lambda'),
             handler = 'hello.handler',
+        )
+
+        # IP制限のポリシーを定義
+        policy = iam.PolicyDocument(
+            statements=[
+                iam.PolicyStatement(
+                    effect=iam.Effect.DENY,
+                    actions=["execute-api:Invoke"],
+                    resources=["*"],
+                    conditions={
+                        "NotIpAddress": {
+                            "aws:SourceIp": "113.43.247.220"
+                        }
+                    },
+                    principals=[iam.ArnPrincipal('*')]
+                )
+            ]
         )
 
         # apiGatewayを用意する
